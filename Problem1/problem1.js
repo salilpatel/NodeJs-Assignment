59bbb174c17e4df8a75c076ed6d4af3d9ff2e6f4
Salil Patel <101462890+salilpatel@users.noreply.github.com>
2022-09-03 15:36:13 +0000

    1 ,2, & 3 integrated

--- Problem1/problem1.js
+++ Problem1/problem1.js
@@ -1,69 +1,17 @@
 const http = require('http');
 const url = require('url');
 
-const data = [
-	{
-		name: 'john',
-		year: 1998,
-		month: 11,
-		date: 23
-	},
-	{
-		name: 'peter',
-		year: 1997,
-		month: 12,
-		date: 2
-	},
-	{
-		name: 'henry',
-		year: 1999,
-		month: 2,
-		date: 28
-	},
-	{
-		name: 'george',
-		year: 1995,
-		month: 1,
-		date: 1
-	},
-	{
-		name: 'kelly',
-		year: 1996,
-		month: 7,
-		date: 9
-	},
-	{
-		name: 'kelly',
-		year: 1996,
-		month: 9,
-		date: 9
-	},
-	{
-		name: 'kelly',
-		year: 1996,
-		month: 12,
-		date: 9
-	}
-];
-
 const server = http.createServer((req, res) => {
 	const parsedURL = url.parse(req.url, true);
 	/*console.log(parsedURL.query)*/
-	const querydatauname = parsedURL.query.name;
-	const querydatayear = parsedURL.query.year;
-	const querydatamonth = parsedURL.query.month;
-	const querydatadate = parsedURL.query.date;
-
 	if (parsedURL.pathname === '/age' && req.method === 'GET') {
-		const result = data.find(
-			item =>
-				item.name == querydatauname &&
-				item.year == querydatayear &&
-				item.month == querydatamonth &&
-				item.date == querydatadate
-		);
-		/*console.log(result)*/
-		if (result) {
+    
+	  const querydatauname = parsedURL.query.name;
+	  const querydatayear = parsedURL.query.year;
+	  const querydatamonth = parsedURL.query.month;
+	  const querydatadate = parsedURL.query.date;
+		
+		if (querydatauname != null && querydatayear != null && querydatamonth != null & querydatadate != null) {
 			res.writeHead(200);
 			let d = new Date();
 			let yearNow = d.getFullYear();
@@ -79,10 +27,7 @@ const server = http.createServer((req, res) => {
 			}
 
 			res.end(
-				JSON.stringify({
-					name: querydatauname,
-					age: age
-				})
+				JSON.stringify(`hello ${querydatauname}, you are ${age} years old.`)
 			);
 		} else {
 			res.writeHead(300);
--- index.js
+++ index.js
@@ -1,55 +1,85 @@
-const http = require('http')
-const url = require('url')
-const data1 = require('./Data/data1.json')
-const data2 = require('./Data/data2.json')
+const http = require('http');
+const url = require('url');
+const data2 = require('./Data/data2.json');
+const data3 = require('./Data/data3.json');
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
+  const parsedURL = url.parse(req.url, true);
+	/* Problem 1 */
+	if (parsedURL.pathname === '/age' && req.method === 'GET') {
     
-    /* problem1 */
-    const parsedURL = url.parse(req.url,true)
-    const querydatauname = parsedURL.query.name
-    const querydatayear = parsedURL.query.year
-    const querydatamonth = parsedURL.query.month
-    const querydatadate = parsedURL.query.date
+	  const querydatauname = parsedURL.query.name;
+	  const querydatayear = parsedURL.query.year;
+	  const querydatamonth = parsedURL.query.month;
+	  const querydatadate = parsedURL.query.date;
+		
+		if (querydatauname != null && querydatayear != null && querydatamonth != null & querydatadate != null) {
+			res.writeHead(200);
+			let d = new Date();
+			let yearNow = d.getFullYear();
+			let monthNow = d.getMonth() + 1;
+			let dateNow = d.getDay();
 
-    if(parsedURL.pathname==="/age" && req.method==="GET"){
-        const result = data1.find((item)=>item.name==querydatauname&&item.year==querydatayear&&item.month==querydatamonth&&item.date==querydatadate)
-        if(result){
+			let age = yearNow - querydatayear;
+			if (
+				monthNow < querydatamonth ||
+				(monthNow == querydatamonth && dateNow < querydatadate)
+			) {
+				age -= 1;
+			}
+
+			res.end(
+				JSON.stringify(`hello ${querydatauname}, you are ${age} years old.`)
+			);
+		} else {
+			res.writeHead(300);
+			res.end(JSON.stringify({ msg: 'data not found' }));
+		}
+	}
+
+	/* Problem 2 */
+	if (parsedURL.pathname === '/vegetables' && req.method === 'GET') {
+		res.writeHead(200);
+		res.end(JSON.stringify(data2));
+	} 
+
+  /* Problem 3 */
+  if(parsedURL.pathname === '/metrics' && req.method === 'GET'){
+        const qdataObject = parsedURL.query.object
+        const qdataMetric = parsedURL.query.metric
+        const qdataRadius = parsedURL.query.radius
+
+        const result = data3.find((item)=>qdataObject===item.object&&qdataMetric===item.metric)
+        if(qdataObject != null && qdataMetric != null && qdataRadius != null){
             res.writeHead(200)
-            let d = new Date();
-            let yearNow = d.getFullYear();
-            let monthNow = d.getMonth() + 1;
-            let dateNow = d.getDay();
-
-            let age = yearNow - querydatayear;
-            if(monthNow < querydatamonth || (monthNow == querydatamonth && dateNow < querydatadate)){
-                age -= 1;
+            let area;
+            /* area of circle */
+            if(result.object  === 'circle' && result.metric === 'area'){
+                area = 3.14 * qdataRadius * qdataRadius
+                area = Math.round(area*100)/100 
             }
-            
-            res.end(JSON.stringify({
-                "name":querydatauname,
-                "age":age
-            }))
-            
+            /* area of sphere */
+            if(result.object  === 'sphere' && result.metric === 'area'){
+                area = 12.56 * qdataRadius * qdataRadius
+                area = Math.round(area*100)/100
+            }
+            /* volume of sphere */
+            if(result.object  === 'sphere' && result.metric === 'volume'){
+                area = 4.19 * qdataRadius * qdataRadius * qdataRadius
+                area = Math.round(area*100)/100
+            }
+            res.end(`${qdataMetric} of ${qdataObject} is ${area}`)
         }
         else{
             res.writeHead(300)
-            res.end(JSON.stringify({"msg":"data not found"}))
+            res.end("enter object, metric, & radius")
         }
     }
-
-    /* Problem2 */
-    if(parsedURL.pathname === '/vegetables' && req.method === 'GET'){
-        res.writeHead(200)
-        res.end(JSON.stringify(data2))
-    }
-
-    /* 404 : page not found */
-    else{
-        res.writeHead(404)
-        res.end('page not found')
-    }
-})
-server.listen(8080,()=>console.log('server started'))
-
-
+  
+  /* 404 : page not found */
+  else {
+		res.writeHead(404);
+		res.end('404 page not found');
+	}
+});
+server.listen(8080, () => console.log('server started'));
